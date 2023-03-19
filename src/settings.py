6495e13ee1b3342c8536f4b3f5622d78485d4eef
pdtangney <peter@peterswebworld.net>
2023-03-19 10:48:40 -0400

    Cleaning up settings module
    This is a clean-up of code I wrote about two years ago.
    
    Cleaning up the Settings class and all its modules. This includes some
    code refactoring and improving doc-strings.
    Some minor variable re-namings, too.
    Also removing the colors() method, as it is un-needed at this point. At
    present, I was using it for a single color variable which is not meant
    to be user-configurable.

--- src/settings.py
+++ src/settings.py
@@ -1,42 +1,67 @@
+"""
+The main setting module for 1982: 8-Bit Life.
+Includes screen resolution, player keybindings,
+player speed settings.
+
+Contains class Settings. This class manages game settings. Most settings
+are user configurable. Where they are not meant to be user configured,
+it will be noted with a comment on the line preceding the setting.
+"""
+
 import pygame
 
 
 class Settings:
-    """Manage settings for block party."""
-    def __init__(self):
-        """Initialize game settings."""
-        # Screen settings
-        self.screenX = 960
-        self.screenY = 540
-        self.resolution = (self.screenX, self.screenY)
-        self.fps = 40
+    """
+    Manage settings for 1982: 8-bit Life.
 
-        self.p1_is_running = False
+    Methods in Settings:
+        initialize_screen() - Set resolution, framerate.
+        player_one_keybindings() - User configurable keyboard controls.
+    """
+    def __init__(self):
+        """Initialize game settings.
+
+           Calls the following public methods:
+                initialize_screen()
+                player_one_keybindings()
+        """
+        self.initialize_screen()
+
+        # Misc player settings. Its is recommended that self.running
+        # is to be set at least twice the speed of self.walking.
         self.walking = 10
         self.running = 20
+        # By default p1 is not running:
+        self.p1_is_running = False
+        self.p1_speed = self.walking
 
         # General game world settings
         self.gravity = 3.2
-        self.forwardX = self.screenX - 100
-        self.backwardX = 50
+        # Screen scrolling settings. The following lines ensure the
+        # player stays in view when approaching the edge of the screen.
+        self.forward_x = self.screen_x - 100
+        self.backward_x = 50
 
         # Keyboard and gamepad bindings.
-        self._player_one_keybindings()
+        self.player_one_keybindings()
 
-    def _player_one_keybindings(self):
-        """Keybindings for player one."""
-        self.p1_speed = self.walking
+    def initialize_screen(self):
+        """
+        Set up the initial screen settings.
+        self.screen_x, self,screen_y are the width, height of
+        the screen.
+        self.fps sets the frame-rate.
+
+        """
+        self.screen_x = 960
+        self.screen_y = 540
+        self.resolution = (self.screen_x, self.screen_y)
+        self.fps = 40
+
+    def player_one_keybindings(self):
+        """Keybindings for player one, when using a keyboard."""
         self.p1_left = pygame.K_LEFT
         self.p1_right = pygame.K_RIGHT
         self.p1_jump = pygame.K_SPACE
         self.p1_run = pygame.K_LSHIFT
-
-    def colors(self, color):
-        if color == 'BLUE':
-            return (25, 25, 200)
-        elif color == 'BLACK':
-            return (23, 23, 23)
-        elif color == 'WHITE':
-            return (254, 254, 254)
-        elif color == 'ALPHA':
-            return (0, 255, 0)
